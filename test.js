cd272a2176dcdc851928a6d37a40d55ad0b52863
Dres90 <acardenas90@gmail.com>
2014-02-27 18:59:33 -0500

    fix test.js

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 var spawn = require('child_process').spawn,
-<<<<<<< HEAD
+
 passTest = spawn('python', ['passTest.py', 'param1', 'param2', 'param3', 'param4']);
-=======
-passTest = spawn('python', ['passTest.py']);
->>>>>>> 4f7974080d2cd64c642afec73b5f4464f46ba614
 
 passTest.stdout.on('data', function(data)
 {
