fdfd7c7ea3616b741b3b19f3dbfa25175b5aeb22
Dres90 <acardenas90@gmail.com>
2014-02-28 18:56:07 -0500

    python fix

--- passTest.py
+++ passTest.py
@@ -13,19 +13,26 @@ import sys
 from django.contrib.auth import hashers
 
 """
+If first argument is "check"
 Used to validate if a password matches a hash using Django hashing functions
 Takes the plain text password and the hash as input.
 Returns 1 if the passwords match
 Returns 0 if the passwords do not match
 Returns -1 if the hashed input password is not a valid hash 
+
+If first argument is "make"
+Used to create a hashed password using Django hashing functions
+Takes the plain text password as input.
+Returns the hash for the input password
+if the input password is null it returns a not valid hash
 """
 if sys.argv[1] == "check":
 
 	raw =  sys.argv[2]
 	hash = sys.argv[3]
 
-	if (hashers.is_password_usable(hash)):
-		if (hashers.check_password(raw,hash)):
+	if hashers.is_password_usable(hash):
+		if hashers.check_password(raw,hash):
 			sys.stdout.write("1")
 			sys.stdout.flush()
 			sys.exit(0)
@@ -38,12 +45,6 @@ if sys.argv[1] == "check":
 		sys.stdout.flush()
 		sys.exit(0)
 
-"""
-Used to create a hashed password using Django hashing functions
-Takes the plain text password as input.
-Returns the hash for the input password
-if the input password is null it returns a not valid hash
-"""		
 elif sys.argv[1] == "make":
 	raw =  sys.argv[2]
 	hash = hashers.make_password(raw)
