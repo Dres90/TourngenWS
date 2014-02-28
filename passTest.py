7377244b104242da7d61fbe05d8f54a5e718e62d
Dres90 <acardenas90@gmail.com>
2014-02-28 17:54:18 -0500

    django fix

--- passTest.py
+++ passTest.py
@@ -1,3 +1,8 @@
+import django
+from django.conf import settings
+from tourngen import tourngen_defaults
+if not settings.configured:
+    settings.configure(myapp_defaults, DEBUG=True)
 import sys
 import django.contrib.auth.hashers
 
